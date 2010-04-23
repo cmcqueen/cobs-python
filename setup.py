4d8b1f6b32e7b14311d3a78c3ba2f61bb9b395f1
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-04-23 09:56:20 +0900

    Set version number to 0.8.0.
    --HG--
    branch : cobsr

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ elif sys.version_info[0] == 3:
 
 setup(
     name='cobs',
-    version='0.8.9',
+    version='0.8.0',
     description='Consistent Overhead Byte Stuffing (COBS)',
     author='Craig McQueen',
     author_email='python@craig.mcqueen.id.au',
