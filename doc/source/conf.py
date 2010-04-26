2ecb5ada6e70d498a2e09e81f37764a0d46d2615
Craig McQueen <craig@mcqueen.id.au>
2010-04-26 21:01:05 +0900

    Switch to pngmath.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.jsmath']
+extensions = ['sphinx.ext.pngmath']
+#extensions = ['sphinx.ext.jsmath']
 jsmath_path = '/home/craig-nat/software/jsMath/easy/load.js'
 
 # Add any paths that contain templates here, relative to this directory.
