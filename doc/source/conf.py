ae9e807c8a3e4c2d1e2983b0886ad2fb61a1d910
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-06-01 13:37:43 +0900

    In Sphinx documentation configuration, update extensions list, with some commented-out matplotlib extensions.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,8 +22,13 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath']
-#extensions = ['sphinx.ext.jsmath']
+extensions = [
+    'sphinx.ext.pngmath',
+#    'sphinx.ext.jsmath',
+#    'matplotlib.sphinxext.mathmpl',
+#    'matplotlib.sphinxext.only_directives',
+#    'matplotlib.sphinxext.plot_directive',
+]
 jsmath_path = 'jsMath/easy/load.js'
 
 # Add any paths that contain templates here, relative to this directory.
