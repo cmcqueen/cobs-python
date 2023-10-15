aec0805f568c7a2ce632de767dd110a79e4b6c94
Craig McQueen <craig.mcqueen@innerrange.com>
2023-10-15 21:48:29 +1100

    Restructure and change packaging, according to current advice
    https://packaging.python.org/en/latest/overview/
    
    Advice on binary extensions is unclear.
    
    https://packaging.python.org/en/latest/guides/packaging-binary-extensions/

--- setup.py
+++ setup.py
@@ -4,42 +4,17 @@ import sys
 
 from setuptools import setup, Extension
 
-import _version
-
 if sys.version_info[0] == 2:
     raise Exception('Python 2.x is no longer supported')
 
 setup_dict = dict(
-    name='cobs',
-    version=_version.__version__,
-    description='Consistent Overhead Byte Stuffing (COBS)',
-    author='Craig McQueen',
-    author_email='python@craig.mcqueen.id.au',
-    url='https://github.com/cmcqueen/cobs-python/',
     packages=[ 'cobs', 'cobs.cobs', 'cobs.cobsr', 'cobs._version', ],
     package_dir={
-        'cobs' : 'python3/cobs',
-        'cobs._version' : '_version',
+        'cobs' : 'src/cobs',
     },
     ext_modules=[
-        Extension('cobs.cobs._cobs_ext', [ 'python3/src/_cobs_ext.c', ]),
-        Extension('cobs.cobsr._cobsr_ext', [ 'python3/src/_cobsr_ext.c', ]),
-    ],
-
-    long_description=open('README.rst').read(),
-
-    license="MIT",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Communications',
+        Extension('cobs.cobs._cobs_ext', [ 'src/ext/_cobs_ext.c', ]),
+        Extension('cobs.cobsr._cobsr_ext', [ 'src/ext/_cobsr_ext.c', ]),
     ],
 )
 
